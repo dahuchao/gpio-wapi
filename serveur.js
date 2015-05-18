6243f79f4b7d44bf0d3374ac033014aaa469ca5d
dahuchao <dahuchao.github@laposte.net>
2015-05-18 22:58:45 +0200

    Ajout des fonctions de l'API web

--- serveur.js
+++ serveur.js
@@ -3,19 +3,51 @@
 var express = require('express');
 // Chargement du module de gestion du système de fichier
 var fs = require('fs');
+// Chargement du module de gestion du port GPIO 
+var gpio = require('rpi-gpio');
+
 // Création de l'application express
 var app = express();
 // Configuration de l'application express
 app.use(express.urlencoded());
 app.use(express.json());
 // Répertoire des pages du site web
-var repertoireSite='C:\\Travail\\Chantiers\\testRaspberry';
+var repertoireSite='.';
 console.log('Ouverture du répertoire des pages du site web : ');
 console.log(repertoireSite);
 if(!fs.existsSync(repertoireSite)){
     console.error('Répertoire des pages indisponible');
 }
 app.use('/lumiere', express.static(repertoireSite));
+
+app.put('/gpio', function(req,res){
+	res.send('Allumage du canal 7.');
+	gpio.setup(7,gpio.DIR_OUT,write);
+	function write(){
+		gpio.write(7,true, function(err){
+			if(err) throw err;
+			console.log('Alimentation du canal 7.');
+		});
+	}
+});
+
+app.get('/allumer', function(req,res){
+	res.send('Allumage du canal 7.');
+	gpio.setup(7,gpio.DIR_OUT,write);
+	function write(){
+		gpio.write(7,true, function(err){
+			if(err) throw err;
+			console.log('Alimentation du canal 7.');
+		});
+	}
+});
+app.get('/eteindre', function(req,res){
+	res.send('Eteindre le canal 7.');
+	gpio.write(7,false,function(err){
+		if(err) throw err;
+		console.log('Extinction du canal 7.');
+	});
+});
 
 //**********************************************
 // Démarrage du serveur
